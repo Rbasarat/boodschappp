52a3997021d26c49f2d595742630f8fa40b4083e
Rasjaad <rasjaad10@msn.com>
2020-10-03 14:10:58 +0200

    Changed capital

--- web/frontend/src/index.tsx
+++ web/frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Home from "./pages/home/Home";
+import Home from "./pages/home/home";
 import * as serviceWorker from "./serviceWorker";
 import { CssBaseline } from "@material-ui/core";
 
